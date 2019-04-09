d6f335843d44023c264107ca7f2080b7c2e873d8
Ariel D <adboy316@yahoo.com>
2019-04-09 18:07:41 +0200

    clab 6

---
+++ clab6/clab6/BubbleGrid.java
@@ -0,0 +1,65 @@
+public class    BubbleGrid {
+    private int[][] grid;
+    private int rows;
+    private int columns;
+    private int gridSize;
+
+    /* Create new BubbleGrid with bubble/space locations specified by grid.
+     * Grid is composed of only 1's and 0's, where 1's denote a bubble, and
+     * 0's denote a space. */
+    public BubbleGrid(int[][] grid) {
+
+        this.grid = grid;
+        this.rows = grid.length;
+        this.columns = grid[0].length;
+        this.gridSize = rows * columns;
+
+
+    }
+
+    /* Returns an array whose i-th element is the number of bubbles that
+     * fall after the i-th dart is thrown. Assume all elements of darts
+     * are unique, valid locations in the grid. Must be non-destructive
+     * and have no side-effects to grid. */
+    public int[] popBubbles(int[][] darts) {
+
+        UnionFind stuckBubbles = findStuckBubbles();
+        int [] dartPositions = translateDartPositions(darts);
+        int [] fallenBubbles = new int[dartPositions.length];
+
+        for (int i = 0; i < fallenBubbles.length; i++) {
+
+           fallenBubbles[i] = stuckBubbles.size(dartPositions[i]);
+        }
+        return fallenBubbles;
+    }
+
+    /* Takes an array of positions in a 2d array, and translates
+     * them into the corresponding position in a 1d array.*/
+    public int [] translateDartPositions(int[][] darts) {
+        int translatedDarts[] = new int [darts.length];
+        for(int i=0; i<darts.length; i++) {
+            for (int j = 0; j < darts[0].length -1; j++) {
+                translatedDarts[i] = (darts[i][j] * columns) + darts[i][j+1];
+            }
+        }
+        return translatedDarts;
+    }
+
+    /* Returns all the connected bubbles.*/
+    public UnionFind findStuckBubbles() {
+
+        UnionFind connections  = new UnionFind(gridSize);
+
+        for (int i = 0; i < columns ; i++) {
+            for (int j = 1; j < rows; j++) {
+                if (grid[0][i] == 1  && grid[0][i] == grid[j][i]){
+                    connections.union(i, (j * columns) + i );
+                } else { break; }
+
+
+            }
+        }
+        return connections;
+    }
+}
---
+++ clab6/clab6/BubbleGridTest.java
@@ -0,0 +1,89 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BubbleGridTest {
+
+    @Test
+    public void testBasic() {
+
+        int[][] grid = {{1, 0, 0, 0},
+                        {1, 1, 1, 0}};
+        int[][] darts = {{1, 0}};
+        int[] expected = {2};
+
+        validate(grid, darts, expected);
+    }
+
+    @Test
+    public void testBiggerGrid () {
+        int[][] grid = {{1, 1, 0},
+                        {1, 0, 0},
+                        {1, 1, 0},
+                        {1, 1, 1}};
+
+        int[][] darts = {{2, 2}, {2, 0}};
+        int[] expected = {0, 4};
+        validate(grid, darts, expected);
+    }
+
+    @Test
+    public void testDisconnectedDrop () {
+        int[][] grid = {{1, 1, 0},
+                {1, 1, 0},
+                {1, 0, 0},
+                {1, 1, 1}};
+
+        int[][] darts = {{1, 1}};
+        int[] expected = {2};
+        validate(grid, darts, expected);
+    }
+
+    @Test
+    public void testFindStuckBubbles () {
+        int[][] grid = {{1, 1, 0},
+                       {1, 1, 0},
+                       {1, 0, 0},
+                       {1, 1, 1}};
+        BubbleGrid sol = new BubbleGrid(grid);
+        UnionFind expectedConnections = new UnionFind(grid.length * grid[0].length);
+
+        assertEquals(12, expectedConnections.count());
+        expectedConnections = sol.findStuckBubbles();
+
+        assertTrue(expectedConnections.connected(0, 3));
+        assertTrue(expectedConnections.connected(0, 6));
+        assertTrue(expectedConnections.connected(0, 9));
+        assertTrue(expectedConnections.connected(1, 4));
+        assertFalse(expectedConnections.connected(4, 7));
+        assertFalse(expectedConnections.connected(4, 10));
+
+    }
+
+
+    @Test
+    public void testTranslateDartPositions() {
+        int[][] grid = {{1, 0, 0, 0},
+                        {1, 1, 1, 0},
+                        {0, 1, 0, 0}};
+        BubbleGrid sol = new BubbleGrid(grid);
+        int[][] darts = {{1, 1},
+                         {2, 1},
+                         {1, 0}};
+        int [] expected = {5, 9, 4};
+        assertArrayEquals(expected, sol.translateDartPositions(darts));
+
+
+        int[][] grid2 = {{1, 0, 0, 0},
+                         {1, 1, 1, 0}};
+        BubbleGrid sol2 = new BubbleGrid(grid2);
+        int[][] darts2 = {{1, 0},   //pos: 4
+                         {1, 1}};  // pos : 5
+        int [] expected2 = {4, 5};
+        assertArrayEquals(expected2, sol2.translateDartPositions(darts2));
+    }
+
+    private void validate(int[][] grid, int[][] darts, int[] expected) {
+        BubbleGrid sol = new BubbleGrid(grid);
+        assertArrayEquals(expected, sol.popBubbles(darts));
+    }
+}
