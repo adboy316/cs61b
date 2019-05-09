156c6f26dbb6c46e4bdf53c0747827338e3c3647
Ariel D <adboy316@yahoo.com>
2019-05-09 20:02:43 +0200

    finished hw4

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -16,6 +16,10 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private List<Vertex> alreadyVisited;
 
 
+    double estimate = 0;
+
+
+
 
     public AStarSolver(AStarGraph<Vertex> input, Vertex
             start, Vertex end, double timeout) {
@@ -24,7 +28,7 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
 
         solution = new ArrayList<>();
         alreadyVisited = new ArrayList<>();
-        solutionWeight = 888.888;
+        solutionWeight = 0.0;
         numStatesExplored = 0;
         DoubleMapPQ<Vertex> PQ = new DoubleMapPQ<>();
         PQ.add(start, input.estimatedDistanceToGoal(start, end));
@@ -32,49 +36,65 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         while (PQ.size() != 0 ) {
             Vertex p = PQ.removeSmallest();
             numStatesExplored += 1;
+            solutionWeight += estimate;
 
-            if (sw.elapsedTime() > timeout) {
-                outcome = SolverOutcome.UNSOLVABLE;
-                solution.clear();
-                timeSpent =  sw.elapsedTime();
-                return;
-            }
-
-            if (p.equals(end)) {
-                solution.add(p);
-                outcome = SolverOutcome.SOLVED;
-                timeSpent =  sw.elapsedTime();
-                return;
-            }
+            if (runOutOfTime(timeout, sw)) return;
+            if (goalReached(end, sw, p)) return;
 
             solution.add(p);
 
-            // Relax edges
-            for (WeightedEdge<Vertex> e : input.neighbors(p)) {
-
-                Double distToP = input.estimatedDistanceToGoal(e.to(), p);
-                Double distToQ = input.estimatedDistanceToGoal(e.to(), e.to());
-                Double w = e.weight();
-
-
-                if (distToP + w < distToQ) {
-                    distToQ = distToP + w;
-                }
-                if (PQ.contains(e.to())) {
-                    PQ.changePriority(e.to(), e.weight() + input.estimatedDistanceToGoal(e.to(), end));
-                }
-                if (!alreadyVisited.contains(e.to()))
-                {
-                    PQ.add(e.to(), distToQ + input.estimatedDistanceToGoal(e.to(), end));
-                    alreadyVisited.add(e.to());
-
-                }
-
-            }
+            estimate = Double.POSITIVE_INFINITY;;
+            relaxEdges(input, end, PQ, p);
         }
         solution.clear();
         outcome = SolverOutcome.UNSOLVABLE;
     }
+
+    private void relaxEdges(AStarGraph<Vertex> input, Vertex end, DoubleMapPQ<Vertex> PQ, Vertex p) {
+        for (WeightedEdge<Vertex> e : input.neighbors(p)) {
+
+            Double distToP = input.estimatedDistanceToGoal(e.to(), p);
+            Double distToQ = input.estimatedDistanceToGoal(e.to(), e.to());
+            Double w = e.weight();
+
+            if (distToP + w < distToQ) {
+                distToQ = distToP + w;
+            }
+            if (PQ.contains(e.to())) {
+                PQ.changePriority(e.to(), e.weight() + input.estimatedDistanceToGoal(e.to(), end));
+            }
+            if (!alreadyVisited.contains(e.to()))
+            {
+                PQ.add(e.to(), distToQ + input.estimatedDistanceToGoal(e.to(), end));
+                alreadyVisited.add(e.to());
+
+                if (e.weight() + distToP  < estimate) {
+                    estimate = e.weight();
+                }
+            }
+        }
+    }
+
+    private boolean goalReached(Vertex end, Stopwatch sw, Vertex p) {
+        if (p.equals(end)) {
+            solution.add(p);
+            outcome = SolverOutcome.SOLVED;
+            timeSpent =  sw.elapsedTime();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean runOutOfTime(double timeout, Stopwatch sw) {
+        if (sw.elapsedTime() > timeout) {
+            outcome = SolverOutcome.UNSOLVABLE;
+            solution.clear();
+            timeSpent =  sw.elapsedTime();
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     public SolverOutcome outcome() {
